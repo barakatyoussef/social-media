63295aa5fbc6f5662cf202238f0c43d2e394f4ed
Youssef Barakat <youssefbarakat892@gmail.com>
2025-04-12 00:49:39 +0100

    Post schema

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ const mongoose = require('mongoose');
 const app = express(); 
 const PORT = process.env.PORT || 5000; 
 
-const authRoutes = require("./routes/authRoutes"); 
-app.use("/api/auth", authRoutes) 
-
 // Connexion à MongoDB 
 mongoose.connect(process.env.MONGO_URI) 
 .then(() => console.log(' MongoDB connecté')) 
@@ -21,7 +18,10 @@ app.use(express.json({ limit: '50mb' })); // Adjust size as needed
 app.use(express.urlencoded({limit: '50mb', extended: true })); 
 app.use(cors()); 
 app.use(helmet()); 
-app.use(morgan('dev')); 
+app.use(morgan('dev'));
+
+const authRoutes = require("./routes/authRoutes"); 
+app.use("/api/auth", authRoutes)
  
 app.get('/', (req, res) => { 
     res.send('Bienvenue sur l’API du réseau social !'); 
