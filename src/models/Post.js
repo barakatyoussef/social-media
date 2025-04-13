0157266c7e9b3ec77a51eba6e962973ab2343500
Youssef Barakat <youssefbarakat892@gmail.com>
2025-04-14 00:39:59 +0100

    ajout du schema du comment et comment dans post

--- src/models/Post.js
+++ src/models/Post.js
@@ -4,7 +4,8 @@ const PostSchema = new mongoose.Schema({
     content : {type: String, required:true},
     image:{type:String},
     author:{type:mongoose.Schema.Types.ObjectId, ref:"User", required:true},
-    likes:[{type:mongoose.Schema.Types.ObjectId, ref:"User"}]
+    likes:[{type:mongoose.Schema.Types.ObjectId, ref:"User"}],
+    comments:[{type:mongoose.Schema.Types.ObjectId, ref:'Comment'}]
 },{timestamps:true});
 
 module.exports= mongoose.model('Post',PostSchema)
