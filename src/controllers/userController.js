32762ff795474c5fa4a29b3f41d507a638b035ba
Youssef Barakat <youssefbarakat892@gmail.com>
2025-04-14 02:44:13 +0100

    ajout des fonction pour following et unfollowing

---
+++ src/controllers/userController.js
@@ -0,0 +1,51 @@
+const User= require('../models/User')
+
+exports.followUser= async(req,res)=>{
+    try{
+        const {userId}=req.params;
+        const currentUserId=req.user.userId
+
+        if(userId=== currentUserId) return res.status(400).json({error:'Vous ne pouvez pas suivre vous-meme'})
+        
+        const userToFollow= await User.findById(userId)
+        const currentUser= await user.findById(currentUserId)
+
+        if(!userToFollow || !currentUser) return res.status(404).json({error:'Utilisateur introuvable'});
+
+        if(currentUser.following.includes(userId)) return res.satatus(404).json({error:'Vous suivez deja cet utilisateur'});
+
+        currentUser.following.push(userId);
+        userToFollow.followers.push(currentUserId);
+
+        await currentUser.save();
+        await userToFollow.save();
+
+        res.json({message:'utilisateur suivi avec succes'})
+
+    }catch(error){
+        res.status(500).json({error:'erreur serveur'})
+    }
+}
+
+exports.unfollowUser= async(req,res)=>{
+    try{
+        const { userId}= req.params
+        const currentUserId= req.user.userId;
+
+        const userToUnfollow= await User.findById(UserId);
+        const currentUser= await User.findById(currentUserId);
+
+        if(!userToUnfollow || !currentUser) return res.status(404).json({error:'utilisateur non trouvable'})
+        
+        currentUser.following = currentUser.following.filter(id=>id.toString()!==userId);
+        userToUnfollow.followers = userToUnfollow.followers.filter(id=>id.toString()!==currentUserId)
+
+        await currentUser.save();
+        await userToUnfollow.save();
+
+        res.json({message:'Utilisateur desuivi avec succes'})
+        
+    }catch(error){
+        res.status(500).json({error:'Erreur serveur'})
+    }
+}
